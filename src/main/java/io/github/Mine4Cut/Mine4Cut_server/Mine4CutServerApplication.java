cd79d7b95a8c8e90efa7b73f426b89574c8f1e88
Doroddi <ibus7180@konkuk.ac.kr>
2025-06-30 12:45:28 +0900

    Initial commit

---
+++ src/main/java/io/github/Mine4Cut/Mine4Cut_server/Mine4CutServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.Mine4Cut.Mine4Cut_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mine4CutServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mine4CutServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Mine4Cut/Mine4Cut_server/Mine4CutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Mine4Cut.Mine4Cut_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mine4CutServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
