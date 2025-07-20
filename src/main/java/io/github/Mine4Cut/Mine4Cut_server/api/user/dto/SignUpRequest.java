97a2286b977bd549e415ea1b55375da87e5791e2
Doroddi <ibus7180@konkuk.ac.kr>
2025-07-21 04:09:05 +0900

    Style: 띄어쓰기 및 오타 수정

--- src/main/java/io/github/Mine4Cut/Mine4Cut_server/api/user/dto/SignUpRequest.java
+++ src/main/java/io/github/Mine4Cut/Mine4Cut_server/api/user/dto/SignUpRequest.java
@@ -3,7 +3,7 @@ package io.github.Mine4Cut.Mine4Cut_server.api.user.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record SingUpRequest (
+public record SignUpRequest(
         @NotBlank @Size(min = 1, max = 50) String username,
         @NotBlank @Size(min = 1, max = 50) String password,
         @NotBlank @Size(min = 2, max = 10) String name,
