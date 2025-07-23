1ee38ae3f3407fcda7f95bac1a33a758ac5fd4b8
Doroddi <ibus7180@konkuk.ac.kr>
2025-07-23 14:14:19 +0900

    Feat: Frame Entity 생성

---
+++ src/main/java/io/github/Mine4Cut/Mine4Cut_server/domain/frame/FrameRepository.java
@@ -0,0 +1,12 @@
+package io.github.Mine4Cut.Mine4Cut_server.domain.frame;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FrameRepository extends JpaRepository<Frame, Long> {
+    List<Frame> findAllByUserId(Long userId);
+    Optional<Frame> findByFrameName(String frameName);
+    boolean existsByFrameName(String frameName);
+}
