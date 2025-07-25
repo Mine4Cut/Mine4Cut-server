3c184bb4f88bb6c7207754ee854f505a141713ca
ekgns33 <ekgns00421@gmail.com>
2025-07-25 22:11:20 +0900

    feat (common) : fix JpaAuditing configuration

--- src/main/java/io/github/Mine4Cut/Mine4Cut_server/Mine4CutServerApplication.java
+++ src/main/java/io/github/Mine4Cut/Mine4Cut_server/Mine4CutServerApplication.java
@@ -2,10 +2,8 @@ package io.github.Mine4Cut.Mine4Cut_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class Mine4CutServerApplication {
 
 	public static void main(String[] args) {
