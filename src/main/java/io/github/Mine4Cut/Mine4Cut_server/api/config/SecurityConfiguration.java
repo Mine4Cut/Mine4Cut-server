02a7852594ef8690ec23264fcc94e008eab71f1e
ekgns33 <ekgns00421@gmail.com>
2025-07-26 17:23:52 +0900

    refactor : 위치 수정

--- src/main/java/io/github/Mine4Cut/Mine4Cut_server/api/config/SecurityConfiguration.java
+++ src/main/java/io/github/Mine4Cut/Mine4Cut_server/api/config/SecurityConfiguration.java
@@ -1,7 +1,10 @@
-package io.github.Mine4Cut.Mine4Cut_server.security.config;
+package io.github.Mine4Cut.Mine4Cut_server.api.config;
 
-import io.github.Mine4Cut.Mine4Cut_server.security.jwt.JwtAuthenticationFilter;
-import io.github.Mine4Cut.Mine4Cut_server.security.jwt.JwtTokenProvider;
+import io.github.Mine4Cut.Mine4Cut_server.api.auth.JwtAuthenticationFilter;
+import io.github.Mine4Cut.Mine4Cut_server.service.auth.JwtTokenProvider;
+import io.github.Mine4Cut.Mine4Cut_server.service.auth.JwtUserService;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,22 +22,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtUserService jwtUserService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtTokenProvider);
+        return new JwtAuthenticationFilter(jwtTokenProvider, jwtUserService);
     }
 
     @Bean
     public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authConfig) throws Exception{
+        AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
 
@@ -46,35 +49,35 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(AbstractHttpConfigurer::disable)
 
-                .sessionManagement(
-                        (sessionManagement) -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                (sessionManagement) -> sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
 
-                .authorizeHttpRequests((registry) -> registry
-                        // 회원가입, 로그인은 모든 사용자 가능
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/users").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/auth/sign-in").permitAll()
-                        .anyRequest().authenticated() // 이외에는 인증된 사용자만
-                )
+            .authorizeHttpRequests((registry) -> registry
+                // 회원가입, 로그인은 모든 사용자 가능
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/sign-in").permitAll()
+                .anyRequest().authenticated() // 이외에는 인증된 사용자만
+            )
 
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 
-                .build();
+            .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList(
-                "${cors.allowed-origins:http://localhost:3000,http://localhost:8080}"));
+        configuration.setAllowedOriginPatterns(List.of(
+            "${cors.allowed-origins:http://localhost:3000,http://localhost:8080}"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
